99443a3cfb396f06505914d847055bf60f2a82bf
Glitch (book-mvp-starter-code) <none>
2022-05-10 16:50:46 +0000

    💘💴 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,17 @@
+//book api url
+//https://www.googleapis.com/books/v1/volumes?q=harry%20potter 
+
+//declare global variables
+
+//include a click handler for search bar
+
+//save user input for book title
+
+//update book api
+
+//add fetch request using book api
+
+//then with function that returns an api response in json
+
+//then with function that displays data to page
+//loop over data to show multiple books
